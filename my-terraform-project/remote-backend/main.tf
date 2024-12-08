cdb90cefbb51eb4a9f87d52978f0a7791459ca6f
RahmaGarg <gargouri.rahma@enis.tn>
2024-12-08 20:54:06 +0100

    Ajout du fichier .gitignore pour ignorer les fichiers Terraform

---
+++ my-terraform-project/remote-backend/main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "custom-terraform-state-bucket-123456-0f4b4cef" # Replace with your S3 bucket name
+    key            = "aws-backend/terraform.tfstate" # Location of the state file in the bucket
+    region         = "us-east-1" # AWS region
+    dynamodb_table = "custom-terraform-state-locks-123456" # Updated DynamoDB table name
+    encrypt        = true # Enables encryption for the state file
+  }
+}
+
+# AWS provider configuration
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Generate a random ID for bucket uniqueness
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+# S3 bucket for storing Terraform state
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "${var.s3_bucket_name}-${random_id.bucket_suffix.hex}"
+  force_destroy = true
+}
+
+# Enable versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption for the S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# DynamoDB table for state locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "custom-terraform-state-locks-123456" # Match the table name with the existing table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
